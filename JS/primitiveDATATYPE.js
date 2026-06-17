767b4641451a04945b8975b1c7841af796cb8fbc
Rachel Kanyoro <peshky38@gmail.com>
2026-06-17 15:20:48 +0300

    Add string

--- JS/primitiveDATATYPE.js
+++ JS/primitiveDATATYPE.js
@@ -11,3 +11,39 @@ let number2 = 42.42;//floating point number
 console.log=(2)
 let number3 = 42//negative number
 console.log=(3)
+//biggest number 1.7976931348623157 x 10^308z
+
+/* strings
+
+3 ways of creating a string
+1.Double quote string
+2.Single quote string
+3.Back Ticks:<Template string>
+*/
+/* 
+special -> /t >tab
+newline -> /n
+single quote escape-> /'
+double quote escape-> /"
+*/
+
+let strl="Double /n quote I am Rachel"//double quote
+console.log(strl)
+let str2='Single Quote I am Rachel'//single quote
+console.log(strl2)
+let str3='Back Ticks I am Rachel'//Back ticks
+console.log(strl3)
+let str4="42"//string or a number:<String>
+console.log(str4)
+/* why do we have multiple ways of representing a string */
+
+let str5 = 'Rachel said "42 is a number"';
+
+let str6 = `
+Multi line string
+Multi line strings
+"" '' both single and double quotes
+Don't
+`;
+
+let str7 = "Don't do this anymore";

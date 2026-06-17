f8117e9b0e06c4137866cee248aa59e50b498175
Rachel Kanyoro <peshky38@gmail.com>
2026-06-17 14:45:16 +0300

    Add primitive js

---
+++ JS/primitiveDATATYPE.js
@@ -0,0 +1,13 @@
+/* NUMBERS
+floating point number:<decimal place>
+real numbers
+positive
+negative numbers
+*/
+
+let number1 =34;//real number
+console.log=(1)
+let number2 = 42.42;//floating point number
+console.log=(2)
+let number3 = 42//negative number
+console.log=(3)

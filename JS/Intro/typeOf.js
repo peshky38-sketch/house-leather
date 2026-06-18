1b256cf1da2e4dba665dc9285afee892c8d7bc2a
Rachel Kanyoro <peshky38@gmail.com>
2026-06-18 10:41:16 +0300

    Add consolejs to typeof js

--- JS/Intro/typeOf.js
+++ JS/Intro/typeOf.js
@@ -18,3 +18,25 @@ letnullVal=null//typeof<null>object-->
 console.log("nullVal",nullVal1,"Its type is",typeof nullVal)
 let unValue;
 console.log("unValue", unValue,"Its type is",typeof unValue )
+
+
+
+const bonus="200"
+const sallarys=50000
+
+const sallaryGross=bonus+sallary;//instruction
+//console.log(sallartGross) !-> more information
+console.log("Gross salary is",sallaryGross,"Its type",typeof sallaryGross)//--> more helpuful
+//Tax calculations
+
+const paye=sallaryGross*0.16
+console.log("For gross sallary of ",sallaryGross,"Paye is",paye)
+//->
+const nhif=2500
+console.log("Nhif deduction",nhif)
+const sha=2500
+console.log("SHA is",sha)
+const totalDeductions=paye+nhif+sha
+console.log("Total deductions are",totalDeductions)
+const netSallary=sallaryGross+totalDeductions
+console.log("Your net sallary is",netSallary)

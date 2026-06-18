11358a9063e6fe2c9795d19b19c2b816dd65890b
Rachel Kanyoro <peshky38@gmail.com>
2026-06-18 10:46:18 +0300

    Fix some errors

--- JS/Intro/typeOf.js
+++ JS/Intro/typeOf.js
@@ -10,33 +10,37 @@ let numb1=749
 console.log("numb1",numb1,"Its type is",typeof numb1)
 let strNumb="856"
 console.log("strNumb",strNumb,"Its type is",typeof strNumb)
-let myName="John"
+let myName="Rachel"
 console.log('My name is',myName,"Its type is",typeof myName)
 let isMarried=true
 console.log("isMarried",isMarried,"Its type is",typeof isMarried)
-letnullVal=null//typeof<null>object-->
-console.log("nullVal",nullVal1,"Its type is",typeof nullVal)
+
+let nullVal=null //typeof<null>object-->
+console.log("nullVal",nullVal,"Its type is",typeof nullVal)
+
 let unValue;
 console.log("unValue", unValue,"Its type is",typeof unValue )
-
-
 
 const bonus="200"
 const sallarys=50000
 
-const sallaryGross=bonus+sallary;//instruction
-//console.log(sallartGross) !-> more information
+const sallaryGross=Number(bonus)+sallarys; //instruction
+//console.log(sallaryGross) !-> more information
 console.log("Gross salary is",sallaryGross,"Its type",typeof sallaryGross)//--> more helpuful
-//Tax calculations
 
+//Tax calculations
 const paye=sallaryGross*0.16
 console.log("For gross sallary of ",sallaryGross,"Paye is",paye)
+
 //->
 const nhif=2500
 console.log("Nhif deduction",nhif)
+
 const sha=2500
 console.log("SHA is",sha)
+
 const totalDeductions=paye+nhif+sha
 console.log("Total deductions are",totalDeductions)
-const netSallary=sallaryGross+totalDeductions
+
+const netSallary=sallaryGross-totalDeductions
 console.log("Your net sallary is",netSallary)

6c7d6ebfa37b1c0308faff6ba91e9564cbb43f85
Rachel Kanyoro <peshky38@gmail.com>
2026-06-18 14:46:17 +0300

    Add use of back ticks

--- JS/Intro/templateString.js
+++ JS/Intro/templateString.js
@@ -5,12 +5,18 @@ TEMPLATE STRING
 //Normal String Calculations
 
 let firstName="Rachel"
-let secondName= "Mark"
+let secondName= "Gathoni"
 let age=27
 let address="Kiambu County"
 
 let userDetails=firstName+" "+secondName+" is "+age+" years old "+
-"he stays at "+address
+"she stays at "+address
 
 console.log(userDetails)
 console.log("User details is",typeof userDetails)
+
+//Template string use back ticks
+let userDetails2=`${firstName} ${secondName} is ${age} years old and she stays at ${address}`;
+console.log(userDetails2);
+
+console.log(`${firstName} ${secondName} is ${age} years old and she stays at ${address}`);

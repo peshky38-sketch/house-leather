9abb649c6a86a5b242a8b044eeffe7f74d55722c
Rachel Kanyoro <peshky38@gmail.com>
2026-06-18 10:04:33 +0300

    Add typeOf.js

---
+++ JS/Intro/typeOf.js
@@ -0,0 +1,20 @@
+/*
+the type of is just to tell you the variable type
+->number
+->string
+->boolean
+->null
+->undefined
+*/
+let numb1=749
+console.log("numb1",numb1,"Its type is",typeof numb1)
+let strNumb="856"
+console.log("strNumb",strNumb,"Its type is",typeof strNumb)
+let myName="John"
+console.log('My name is',myName,"Its type is",typeof myName)
+let isMarried=true
+console.log("isMarried",isMarried,"Its type is",typeof isMarried)
+letnullVal=null//typeof<null>object-->
+console.log("nullVal",nullVal1,"Its type is",typeof nullVal)
+let unValue;
+console.log("unValue", unValue,"Its type is",typeof unValue )

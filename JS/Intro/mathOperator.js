564cde064dfdd4ee2e6bb024219a4853bebf2620
Rachel Kanyoro <peshky38@gmail.com>
2026-06-18 15:22:57 +0300

    Add mathOperator.js

---
+++ JS/Intro/mathOperator.js
@@ -0,0 +1,23 @@
+/*
+Assignment=
+*/
+let numb1=32;//Declared and assigned the variable
+console.log(`numb1 is ${numb1} its type is ${typeof numb1}`)
+
+numb1=30; //Assignment only
+console.log(`numb1 is ${numb1} its type is ${typeof numb1}`)
+
+let numb2=43;
+console.log(`numb2 is ${numb2} its type is ${typeof numb2}`)
+
+let numb3=numb1+numb2+10//30+43+10= > 83
+console.log(`numb3 is ${numb3} its type is ${typeof numb3}`)
+
+let numb4=numb1*numb2
+console.log(`numb4 is ${numb4} its type is ${typeof numb4}`)
+
+let numb5=numb1**3//2*2*2 -->power 2*2*2
+console.log(`numb5 is ${numb5} its type is ${typeof numb5}`)
+
+let numb6=Math.pow(10,3)//2^3
+console.log(`numb6 is ${numb6} its type is ${typeof numb6}`)

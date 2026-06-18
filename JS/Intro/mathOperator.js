815c8ad90da456b5149f3a207f5f70051bcd92c6
Rachel Kanyoro <peshky38@gmail.com>
2026-06-18 15:46:39 +0300

    Add division and modulus

--- JS/Intro/mathOperator.js
+++ JS/Intro/mathOperator.js
@@ -1,5 +1,8 @@
 /*
 Assignment=
+Addition
+Division/
+
 */
 let numb1=32;//Declared and assigned the variable
 console.log(`numb1 is ${numb1} its type is ${typeof numb1}`)
@@ -21,3 +24,20 @@ console.log(`numb5 is ${numb5} its type is ${typeof numb5}`)
 
 let numb6=Math.pow(10,3)//2^3
 console.log(`numb6 is ${numb6} its type is ${typeof numb6}`)
+
+//
+let numb7=90
+let numb8=30
+let divNumb=numb7/numb8
+console.log(`divNumb is ${divNumb} its type is ${typeof divNumb}`)
+
+//Modulus is somehow special
+//divide 2 numbers -> remainder
+let numb9=55
+let numb10=6
+let modNu=numb9%numb10
+console.log(`modNu is ${modNu} its type is ${typeof modNu}`)
+
+
+
+

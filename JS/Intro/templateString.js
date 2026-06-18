ecae68c329a6efa7679a074e427bec944d27d9b6
Rachel Kanyoro <peshky38@gmail.com>
2026-06-18 14:33:44 +0300

    Fix spacing

---
+++ JS/Intro/templateString.js
@@ -0,0 +1,16 @@
+/*
+TEMPLATE STRING
+
+*/
+//Normal String Calculations
+
+let firstName="Rachel"
+let secondName= "Mark"
+let age=27
+let address="Kiambu County"
+
+let userDetails=firstName+" "+secondName+" is "+age+" years old "+
+"he stays at "+address
+
+console.log(userDetails)
+console.log("User details is",typeof userDetails)

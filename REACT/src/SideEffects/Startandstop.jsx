5d8a27fd079381735fe38cd1761fcb6855c06d97
Rachel Kanyoro <peshky38@gmail.com>
2026-08-06 11:14:59 +0300

    Fix seconds

--- REACT/src/SideEffects/Startandstop.jsx
+++ REACT/src/SideEffects/Startandstop.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 
 function Startandstop() {
-  const [seconds, setSeconds] = useState(0);
+ const [seconds, setSeconds] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
   const intervalRef = useRef(null);
 

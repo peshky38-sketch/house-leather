ec47642e10e6d12ff0f195097b2b6d6874882297
Rachel Kanyoro <peshky38@gmail.com>
2026-08-25 10:40:20 +0300

    Start PYTHON practice

---
+++ PYTHON/Intro/data_type.py
@@ -0,0 +1,25 @@
+# Example variables
+
+x = 10  # Integer
+y = 3.14  # Float
+z = "Hello"  # String
+a = True  # Boolean <True, False>
+b = [1, 2, 3, 4]  # List <Array> Mutable <By value>
+c = {1, 2, 3}  # Set
+d = (1, 2, 3)  # Tuple <List> Immutable
+e = {"key": "value"}  # Dictionary <Object.js>
+
+# Dictionary <Object.js>
+# For dictionary use bracket notation
+
+# Determining the types
+# y is {}
+
+print("X is", x, "Its type", type(x))
+print(f"Y is {y} its type is {type(y)}")
+print(type(z))
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))

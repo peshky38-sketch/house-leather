74a4ee3bfe55a2b7f584e22cc6bb798d64e8cfc5
Rachel Kanyoro <peshky38@gmail.com>
2026-09-03 10:14:07 +0300

    Advance the class object oriented

--- PYTHON/object_oriented_programming/bank/account.py
+++ PYTHON/object_oriented_programming/bank/account.py
@@ -20,19 +20,52 @@ class BankAccount:
 
     def __init__(self, name, balance, account_no):
         self.name = name
-        self.balance = balance
         self.account_no = account_no
+        self._balance = 0
+        self.balance = balance
 
-    # latter change gee
-    def get_balance(self):
-        pass
+    @property
+    def balance(self):
+        print("somebody tried to read rachels balance")
+        return self._balance
 
-    # setter
-    def deposit(self):
-        pass
+    @balance.setter
+    def balance(self, value):
+        if not isinstance(value, (int, float)):
+            print("Ensure you pass a number for new balance")
+            return
 
-    def withdrawal(self):
-        pass
+        if value < 0:
+            print("Ensure new balance must not be less than 0")
+            return
+
+        self._balance = value
+
+    def deposit(self, amount):
+        if not isinstance(amount, (int, float)):
+            print("Deposit amount must be a number")
+            return
+
+        if amount <= 0:
+            print("Deposit amount must be greater than 0")
+            return
+
+        self.balance += amount
+
+    def withdrawal(self, amount):
+        if not isinstance(amount, (int, float)):
+            print("Withdrawal amount must be a number")
+            return
+
+        if amount <= 0:
+            print("Withdrawal amount must be greater than 0")
+            return
+
+        if amount > self.balance:
+            print("Insufficient funds")
+            return
+
+        self.balance -= amount
 
     def show_account_details(self):
         print(f"Owner {self.name}")
@@ -41,5 +74,8 @@ class BankAccount:
 
 
 rachel = BankAccount(name="Rachel Gathoni", balance=0, account_no="223344226")
-
+print("Rachel balance", rachel.balance)
+rachel.deposit(5000)
+rachel.withdrawal(2000)
 rachel.show_account_details()
+print("It is easily accessible")
